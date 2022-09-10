8381e68c6a0529ff62346d8649d90f5d2ce3b603
MarlonAndresBT <mbarretot@hotmail.com>
2022-09-10 16:37:19 -0500

    cors issues

--- luzu-api/index.js
+++ luzu-api/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const routes = require("./src/routes");
 
 const port = process.env.PORT || 3001
 const server = express()
+
+server.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+	next();
+});
+server.use(cors());
 
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
